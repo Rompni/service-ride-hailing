45079f41383afe7b16b1de4744f612116ab93a32
rompni <andresnavarrodev@gmail.com>
2023-05-29 07:56:08 -0500

    list of enums to use on models

---
+++ src/utils/enums.ts
@@ -0,0 +1,16 @@
+enum StatusEnumType {
+  AVAILABLE = 'available',
+  ONARIDE = 'onaride',
+}
+
+enum RoleEnumType {
+  RIDER = 'rider',
+  DRIVER = 'driver',
+}
+
+enum PaymentMethodTypeEnum {
+  CARD = 'CARD',
+  NEQUI = 'NEQUI',
+}
+
+export { StatusEnumType, RoleEnumType, PaymentMethodTypeEnum }
