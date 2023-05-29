f66817351a9292c6d98c30a6789ad638fdfc7422
rompni <andresnavarrodev@gmail.com>
2023-05-29 07:53:42 -0500

    config eslint and prettier

--- config/custom-environment-variables.ts
+++ config/custom-environment-variables.ts
@@ -1,16 +1,16 @@
 export default {
-  port: "PORT",
-  postgresConfig: {
-    host: "POSTGRES_HOST",
-    port: "POSTGRES_PORT",
-    username: "POSTGRES_USER",
-    password: "POSTGRES_PASSWORD",
-    database: "POSTGRES_DB",
-  },
-  wompiConfig: {
-    publicKey: "WOMPI_PUBLIC_KEY",
-    privateKey: "WOMPI_PRIVATE_KEY",
-    baseUrl: "WOMPI_BASE_URL",
-    acceptationToken: "WOMPI_ACCEPTATION_TOKEN",
-  },
-};
+    port: 'PORT',
+    postgresConfig: {
+        host: 'POSTGRES_HOST',
+        port: 'POSTGRES_PORT',
+        username: 'POSTGRES_USER',
+        password: 'POSTGRES_PASSWORD',
+        database: 'POSTGRES_DB',
+    },
+    wompiConfig: {
+        publicKey: 'WOMPI_PUBLIC_KEY',
+        privateKey: 'WOMPI_PRIVATE_KEY',
+        baseUrl: 'WOMPI_BASE_URL',
+        acceptationToken: 'WOMPI_ACCEPTATION_TOKEN',
+    },
+}
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,22 +1,22 @@
-require("dotenv").config();
-import "reflect-metadata";
-import { DataSource } from "typeorm";
-import config from "config";
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+import config from 'config'
+require('dotenv').config()
 
 const postgresConfig = config.get<{
-  host: string;
-  port: number;
-  username: string;
-  password: string;
-  database: string;
-}>("postgresConfig");
+  host: string
+  port: number
+  username: string
+  password: string
+  database: string
+}>('postgresConfig')
 
 export const AppDataSource = new DataSource({
   ...postgresConfig,
-  type: "postgres",
+  type: 'postgres',
   synchronize: false,
   logging: true,
-  entities: ["src/entities/**/*.entity{.ts,.js}"],
-  migrations: ["src/migrations/**/*{.ts,.js}"],
-  subscribers: ["src/subscribers/**/*{.ts,.js}"],
-});
+  entities: ['src/models/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/**/*{.ts,.js}'],
+  subscribers: ['src/subscribers/**/*{.ts,.js}']
+})
